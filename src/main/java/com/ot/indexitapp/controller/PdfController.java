7d76dc7158ae4e9e4f17ad14ae0940bd16159912
Srinivas Chintakindhi <srinivas.c@ayu.health>
2024-05-13 15:45:59 +0530

    backend api to fetch serve pdf file

--- src/main/java/com/ot/indexitapp/controller/PdfController.java
+++ src/main/java/com/ot/indexitapp/controller/PdfController.java
@@ -1,12 +1,18 @@
 package com.ot.indexitapp.controller;
 
 import com.ot.indexitapp.service.PdfFileService;
+import org.springframework.core.io.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,4 +26,25 @@ public class PdfController {
         List<String> files = fileService.listPdfFiles(path);
         return ResponseEntity.ok(files);
     }
+
+    @GetMapping("/api/pdf/files/{filename}")
+    public ResponseEntity<Resource> getFile(
+            @PathVariable String filename,
+            @RequestParam(required = false, defaultValue = "default/path") String path,
+            HttpServletRequest request
+    ) {
+        Resource resource = fileService.loadFileAsResource(path, filename);
+
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(resource);
+    }
+
 }
--- src/main/java/com/ot/indexitapp/service/PdfFileService.java
+++ src/main/java/com/ot/indexitapp/service/PdfFileService.java
@@ -1,8 +1,13 @@
 package com.ot.indexitapp.service;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,5 +21,20 @@ public class PdfFileService {
                 .filter(file -> file.isFile() && file.getName().endsWith(".pdf"))
                 .map(File::getName)
                 .collect(Collectors.toList());
+    }
+
+    public Resource loadFileAsResource(String directoryPath, String fileName) {
+        try {
+            Path fileStorageLocation = Paths.get(directoryPath).toAbsolutePath().normalize();
+            Path filePath = fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new RuntimeException("File not found " + fileName);
+            }
+        } catch (MalformedURLException ex) {
+            throw new RuntimeException("File not found " + fileName, ex);
+        }
     }
 }
